717a6ebacfcc50277e6c79776f2423bc19def17d
Artem <artem.rusyaukin.03@mail.ru>
2021-12-10 09:17:55 +0300

    Добавил 3 кнопки

---
+++ Zadanie_1.2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using AnimatedWindow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAW_BLEND_Click(object sender, EventArgs e)
+        {
+            // Скрываем окно
+            this.Hide();
+            // Запускаем анимацию.
+            //Второй параметр в скобках время анимации в
+            //миллисекундах
+
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_ACTIVATE | WinApiClass.AnimateWindowFlags.AW_BLEND);
+            // Отображаем кнопки после анимации
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_HOR_POSITIVE | WinApiClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_CENTER | WinApiClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
